486e2004762242ee0aeea774722312e033469a29
Adnan-cse-00 <ahosanulkabiradnan@gmail.com>
2026-01-22 08:40:25 +0600

    1

---
+++ main_snippet.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define YES cout << "YES";
+#define NO cout << "NO";
+#define ll long long
+#define ull unsigned long long
+#define nn '\n'
+#define LM LLONG_MAX
+#define IM INT_MAX
+#define ie 1000000
+#define BE .begin()
+#define EN .end()
+#define vel vector<ll>
+#define BB bool
+#define NN NULL
+#define F .find
+#define SI .size()
+#define FST ->first
+#define SEC ->second
+#define all(x) (x).begin(),(x).end()
+void Take(vel &v) {
+    for (auto &i : v) {
+        cin >> i;
+    }
+}
+void Out(const vel &v) {
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << nn;
+}
+
+int solve() {
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    solve();
+    return 0;
+}
